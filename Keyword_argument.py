2478a846d39a24428709de7ee5b2f673b74d635e
uvesh_nagani <164164923+naganiuvesh@users.noreply.github.com>
2026-04-04 11:14:49 +0530

    Update Keyword_argument.py

--- Keyword_argument.py
+++ Keyword_argument.py
@@ -2,16 +2,14 @@
 def student_info (name, age, city) :
     print("Name :", name)
     print("Age :", age)
-    print("City :", city)
-    
+    print("City :", city)    
 student_info (age = 18, city = "Rajkot", name = "Ravi")
 
 # Mixing Positional and Keyword
 def display (a, b, c) :
     print("a", a)
     print("b", b)
-    print("c", c)
-    
+    print("c", c)    
 display(1, c = 3, b = 2)
 
 
